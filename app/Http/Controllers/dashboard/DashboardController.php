254273bc4ad3b1a2879848c84b4e7bb269f3d433
jooprima <jooprima@gmail.com>
2021-02-11 22:29:42 +0700

    Menu active

--- app/Http/Controllers/dashboard/DashboardController.php
+++ app/Http/Controllers/dashboard/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $active='Aktif!!!';
+        $active='Dashboard';
 
         return view('home', ['active'=>$active]);
     }
--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,6 +1,7 @@
 <nav class="nav flex-column">
     @foreach ($list as $row)
-        <a class="nav-link" href="#">
+        <a class="nav-link @if ($row['label']==$active) active @endif"
+            href="#">
             {{ $row['label'] }}
         </a>
     @endforeach
