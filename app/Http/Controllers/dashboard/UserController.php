d27601a385f922622e14c6a2ec4b1389dfa3f2a6
jooprima <jooprima@gmail.com>
2021-02-16 10:38:15 +0700

    Auth di dalam controller

--- app/Http/Controllers/dashboard/UserController.php
+++ app/Http/Controllers/dashboard/UserController.php
@@ -9,6 +9,11 @@ use Validator;
 
 class UserController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
